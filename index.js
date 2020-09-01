62bb36765ab6ca4ad18eef2c93aed32e0d7aa213
Justin Langlinais <justinclanglinais@gmail.com>
2020-09-01 16:12:03 -0700

    solving

--- index.js
+++ index.js
@@ -2,6 +2,18 @@ function getName(node) {
     return node.name
 }
 
-function headNode(node) {
-    
+function headNode(linkedList, collection) {
+    return collection[linkedList]
+}
+
+function next(node, collection) {
+    let nextAddress = node.next
+    return collection[`${nextAddress}`]
+}
+
+function nodeAt(index, node, collection) {
+    for (let i = index; i > 0; i--) {
+        next(node, collection)
+    }
+    return
 }
